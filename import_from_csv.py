b279cae9c0852bb78f3af685174788f74081d2bd
Jeffrey Chang <jeffreychang@Jeffreys-iMac.local>
2016-04-01 21:29:53 -0400

    Initial commit

--- import_from_csv.py
+++ import_from_csv.py
@@ -14,24 +14,33 @@ def open_csv(file):
         for row in reader:
             x_axis.append(row['gestational_age'])
             y_axis.append(row['abdominal_circumference'])
-
             feature_string += row['gestational_age'] + ';'
             result_string += row['abdominal_circumference'] + ';'
+
+        #Strings used for creating the matrix using the string populated from the csv file.
+
         feature_string = feature_string[0:len(feature_string)-1]
         result_string = result_string[0:len(result_string) - 1]
         feature_vector = numpy.matrix(feature_string)
         result_vector = numpy.matrix(result_string)
-        import matplotlib.pyplot
-        pyplot.plot(x_axis, y_axis, 'ro')
-        pyplot.axis([0, 100, 200, 300])
-        pyplot.show()
+
+
+
+        show_plot(x_axis,y_axis)
+
         return (feature_vector, result_vector)
 
 
+def show_plot(x_axis, y_axis):
+    linear_regression_model = numpy.linspace(0, 100, 100)
 
+    pyplot.plot(x_axis, y_axis, 'r.')
+    pyplot.axis()
 
+    pyplot.plot(8.49189782 * linear_regression_model)
 
-
-
+    pyplot.xlabel('Gestational Age')
+    pyplot.ylabel('Abdominal Circumference')
+    pyplot.show()
 
 
--- main.py
+++ main.py
@@ -5,7 +5,10 @@ patient_data = open_csv('data/heart_data.csv')
 theta = patient_data[0]
 results = patient_data[1]
 
-print((linalg.pinv(theta.transpose() * theta)) * (theta.transpose() * results))
+# The minimum of cost function j
+
+
+
 
 
 
